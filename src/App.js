364417915d72379803302b5f810c1a262b75d6ca
Andy Lee <techandy42@gmail.com>
2021-08-03 11:18:46 -0500

    auth service in front end

--- src/App.js
+++ src/App.js
@@ -1,72 +1,17 @@
 import { useState, useMemo } from 'react'
 
-//only store while the user is using the application
+//Create a login system that persists after reloading or rebooting of the application
+
+import Registration from './components/Registration'
+import Login from './components/Login'
 
 function App() {
-  const [info, setInfo] = useState({ firstName: '', lastName: '', age: 0 })
-  if (!Object.keys(localStorage).includes('num')) {
-    localStorage.setItem('num', 1)
-  }
-  const num = parseInt(localStorage.getItem('num'))
-
-  const handleSubmitInfo = (e) => {
-    e.preventDefault()
-    sessionStorage.setItem(`firstName${num}`, info.firstName)
-    sessionStorage.setItem(`lastName${num}`, info.lastName)
-    sessionStorage.setItem(`ageName${num}`, info.age)
-    setInfo({ firstName: '', lastName: '', age: 0 })
-    localStorage.setItem('num', num+1)
-  }
-
-  let infos = Object.entries(sessionStorage)
-  let groupInfos = []
-  let group = []
-
-  groupInfos = useMemo(() => {
-    for (let i=0;i<infos.length-1;i++) {
-      let sorted = true
-      for (let j=0;j<infos.length-1;j++) {
-        if (parseInt(infos[j][0][infos[j][0].length-1]) > parseInt(infos[j+1][0][infos[j+1][0].length-1])) {
-          let temp = infos[j]
-          infos[j] = infos[j+1]
-          infos[j+1] = temp
-          sorted = false
-        }
-      }
-      if (sorted) {
-        break
-      }
-    }
-    for (let i=0;i<infos.length;i++) {
-      group.push(infos[i])
-      if (i%3===2) {
-        groupInfos.push(group)
-        group = []
-      }
-    }
-    console.log(groupInfos)
-    return groupInfos
-  }, [num])
-
+  const [counter, setCounter] = useState(0)
 
   return (
     <div>
-      <form onSubmit={handleSubmitInfo}>
-        <input type='text' value={info.firstName} onChange={e => setInfo({ ...info, firstName: e.target.value })} />
-        <input type='text' value={info.lastName} onChange={e => setInfo({ ...info, lastName: e.target.value })} />
-        <input type='number' value={info.age} onChange={e => setInfo({ ...info, age: parseInt(e.target.value) })} />
-        <button type='submit'>Submit</button>
-      </form>
-      {groupInfos.map(group => {
-        return (
-          <div>
-            <p>{group[0][1]}</p>
-            <p>{group[1][1]}</p>
-            <p>{group[2][1]}</p>
-          </div>
-        )
-      })}
-      <p>{num}</p>
+      <Registration counter={counter} setCounter={setCounter} />
+      <Login counter={counter} />      
     </div>
   )
 }
